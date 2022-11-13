f95261dd6622913131ba6456e68f8b5586b87770
rogerio12m <rogerio.s0205@gmail.com>
2022-11-12 22:48:55 -0300

    implementação do método validarCnpj

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BackEnd.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace BackEnd.Classes
 {
@@ -13,7 +14,30 @@ namespace BackEnd.Classes
 
         public bool ValidarCnpj(string parCnpj)
         {
-            throw new NotImplementedException();
+            // 04.419.296/0001-30 - 18
+            // 04419296000130 - 14
+
+            if (Regex.IsMatch(parCnpj, @"^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$"))
+            {
+                // valida se tem o mil contra 0001
+                if ( parCnpj.Length == 18 )
+                {
+                    if (parCnpj.Substring(11, 4) == "0001")
+                    {
+                        return true;
+                    }
+                } 
+                else if( parCnpj.Length == 14)
+                {
+                    if (parCnpj.Substring(8, 4) == "0001")
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return false; 
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,44 @@
 ﻿using BackEnd.Classes;
+using System.Text.RegularExpressions;
+
+
+// *********************SUBSTRING**************************
+//  0123456789...
+// string nome = "Rogério Moreira da Silva";
+
+// System.Console.WriteLine(nome.Length);
+// System.Console.WriteLine(nome.Substring(8));
+// System.Console.WriteLine(nome.Substring(0, 7));
+
+// System.Console.WriteLine(nome.IndexOf("Moreira"));
+
+// Console.WriteLine(@$"
+//     Nome completo: {nome}
+//     Sobrenome: {nome.Substring(nome.IndexOf("Moreira"))}
+// ");
+
+
+// ********************* REGEX ************************
+// string data = "26/10/2022";
+// bool dataValida = Regex.IsMatch(data, @"^\d{2}/\d{2}/\d{4}$");
+
+// Console.WriteLine(dataValida);
+
+
 
 //endereço de pessoa física
-Endereco endPf = new Endereco();
-    endPf.logradouro = "Rua de Casa";
-    endPf.numero = 123;
-    endPf.comercial = false;
+// Endereco endPf = new Endereco();
+//     endPf.logradouro = "Rua de Casa";
+//     endPf.numero = 123;
+//     endPf.comercial = false;
 
-//pessoa física
-PessoaFisica novaPessoaFisica = new PessoaFisica();
+// //pessoa física
+// PessoaFisica novaPessoaFisica = new PessoaFisica();
 
-    novaPessoaFisica.nome = "Rogério";
-    novaPessoaFisica.endereco = endPf;
-    novaPessoaFisica.rendimento = 1000f;
-    novaPessoaFisica.cpf = "123456789-00";
+//     novaPessoaFisica.nome = "Rogério";
+//     novaPessoaFisica.endereco = endPf;
+//     novaPessoaFisica.rendimento = 1000f;
+//     novaPessoaFisica.cpf = "123456789-00";
 
 //endereço de pessoa jurídica
 Endereco endPj = new Endereco();
@@ -25,22 +51,26 @@ PessoaJuridica novaPessoaJuridica = new PessoaJuridica();
     novaPessoaJuridica.nome = "Paulo Skaf";
     novaPessoaJuridica.endereco = endPj;
     novaPessoaJuridica.razaoSocial = "Serviço Nacional de Aprendizagem Industrial";
-    novaPessoaJuridica.cnpj = "70.469.056/0001-75";
+    // novaPessoaJuridica.cnpj = "70.469.056/0001-75";
+    novaPessoaJuridica.cnpj = "70469056000175";
     novaPessoaJuridica.rendimento = 100000f;
 
-Console.Clear();
+    Console.WriteLine($"CNPJ Válido? {novaPessoaJuridica.ValidarCnpj(novaPessoaJuridica.cnpj)}");
+    
 
-//Exibindo dados da pessoa física
-Console.WriteLine($"******* PESSOA FÍSICA *******");
-Console.WriteLine();
+// Console.Clear();
 
-Console.WriteLine($"Nome: {novaPessoaFisica.nome}");
-Console.WriteLine($"Endereço: {novaPessoaFisica.endereco.logradouro}");
-Console.WriteLine($"Número: {novaPessoaFisica.endereco.numero}");
-Console.WriteLine($"Endereço Comercial: {novaPessoaFisica.endereco.comercial}");
-Console.WriteLine($"CPF: {novaPessoaFisica.cpf}");
-Console.WriteLine($"Rendimento: R$ {novaPessoaFisica.rendimento}");
-Console.WriteLine("Maior de Idade:" + novaPessoaFisica.ValidarDataNascimento("02/05/2004"));
+// //Exibindo dados da pessoa física
+// Console.WriteLine($"******* PESSOA FÍSICA *******");
+// Console.WriteLine();
+
+// Console.WriteLine($"Nome: {novaPessoaFisica.nome}");
+// Console.WriteLine($"Endereço: {novaPessoaFisica.endereco.logradouro}");
+// Console.WriteLine($"Número: {novaPessoaFisica.endereco.numero}");
+// Console.WriteLine($"Endereço Comercial: {novaPessoaFisica.endereco.comercial}");
+// Console.WriteLine($"CPF: {novaPessoaFisica.cpf}");
+// Console.WriteLine($"Rendimento: R$ {novaPessoaFisica.rendimento}");
+// Console.WriteLine("Maior de Idade:" + novaPessoaFisica.ValidarDataNascimento("02/05/2004"));
 
 //Exibindo dados da pessoa jurídica
 // Console.WriteLine($"******* PESSOA JURÍDICA *******");
@@ -53,6 +83,7 @@ Console.WriteLine("Maior de Idade:" + novaPessoaFisica.ValidarDataNascimento("02
 // Console.WriteLine($"Número: {novaPessoaJuridica.endereco.numero}");
 // Console.WriteLine($"Endereço Comercial: {novaPessoaJuridica.endereco.comercial}");
 // Console.WriteLine($"Rendimento: R$ {novaPessoaJuridica.rendimento}");
+
 
 
 
